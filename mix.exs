fd8377d8ca1a97acf5503c54ffcbb40a9abc94e0
kevin <mail@kseidel.org>
2021-10-11 10:39:49 +0200

    Update version in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tds.Encoding.MixProject do
     [
       app: :tds_encoding,
       compilers: Mix.compilers(),
-      version: "1.0.0",
+      version: "1.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
